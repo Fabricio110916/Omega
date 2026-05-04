0816508626a8b45d24f0a4277af06128622360ef
Fabricio110916 <fabricio.junior@edgeclaro.com.br>
2026-05-04 12:32:27 -0400

    Update proxy.js to set headers correctly

--- api/proxy.js
+++ api/proxy.js
@@ -7,9 +7,15 @@ export default async function handler(req) {
 
   const target = "https://my.koom.pp.ua" + url.pathname + url.search;
 
+  const headers = new Headers(req.headers);
+
+  // força host correto (muito importante)
+  headers.set("host", "my.koom.pp.ua");
+
   return fetch(target, {
     method: req.method,
-    headers: req.headers,
-    body: req.body
+    headers,
+    body: req.body,
+    redirect: "manual"
   });
 }
